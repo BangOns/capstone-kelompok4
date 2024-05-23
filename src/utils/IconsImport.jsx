7050eb88b7f3a7df254d32a753102a8bc5ab2a5b
BangOns <syahr9950@gmail.com>
2024-05-23 21:40:30 +0700

    docs: Membuat Struktur Folder

---
+++ src/utils/IconsImport.jsx
@@ -0,0 +1,6 @@
+import IconsDocument from "@/assets/icons/Icons-Document.svg";
+
+export const Iconsmport = {
+  // masukkan icon di sini
+  IconsDocument,
+};
---
+++ src/utils/ImageImport.jsx
@@ -0,0 +1,5 @@
+import ImageTest from "@/assets/image/Image-Test.png";
+export const ImageImport = {
+  // masukkan image disini
+  ImageTest,
+};
