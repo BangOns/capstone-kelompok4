f427bfcdaacd1c64e7685c5252aaa62f8071de33
BangOns <syahr9950@gmail.com>
2024-05-25 23:49:35 +0700

    feat: Slicing Manage Plant

--- src/utils/IconsImport.jsx
+++ src/utils/IconsImport.jsx
@@ -2,6 +2,10 @@ import IconsDocument from "@/assets/icons/Icons-Document.svg";
 import IconsLogoPlantopia from "@/assets/icons/Icons-Logo-Plantopia.svg";
 import IconsSearch from "@/assets/icons/Icons-Search.svg";
 import IconsAddPlant from "@/assets/icons/Icons-Add.svg";
+import IconsMenuTable from "@/assets/icons/Icons-Menu-Table.svg";
+import IconsViewDetails from "@/assets/icons/Icons-View-Details.svg";
+import IconsDeletePlants from "@/assets/icons/Icons-Delete-Plant.svg";
+import IconsDropdown from "@/assets/icons/Icons-Dropdown.svg";
 
 export const IconsImport = {
   // masukkan icon di sini
@@ -9,4 +13,8 @@ export const IconsImport = {
   IconsLogoPlantopia,
   IconsSearch,
   IconsAddPlant,
+  IconsMenuTable,
+  IconsViewDetails,
+  IconsDeletePlants,
+  IconsDropdown,
 };
