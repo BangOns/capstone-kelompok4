f3e9a1dd37fdf49ff4b0bf42d92ebfa555f0632b
elsiip <elsi.puspita1@gmail.com>
2024-05-28 00:23:20 +0700

    style: fix a font italic at plant caring page

--- src/components/Dashboard/Component_Add_plant/List_Component_Step_Component/Plant_Caring.jsx
+++ src/components/Dashboard/Component_Add_plant/List_Component_Step_Component/Plant_Caring.jsx
@@ -12,7 +12,7 @@ export default function Plant_Caring() {
             <div className="border rounded-md border-neutral-200 p-4">
                 <div className="flex flex-row gap-2 mb-6 items-center">
                     <p className="text-xl font-nunito-bold">Additional Planting Instructions Tips </p>
-                    <p className="font-nunito-light text-slate-500"> (if necessary) </p>
+                    <p className="font-nunito-italic text-slate-500"> (if necessary) </p>
                 </div>
                 <ReactQuill
                     className="w-full h-[249px] border border-neutral-400 flex-col-reverse flex overflow-hidden rounded"
