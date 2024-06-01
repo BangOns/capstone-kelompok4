c196af15fdf14fe85881e41a82ba3723ce7cb03d
BangOns <syahr9950@gmail.com>
2024-06-01 13:18:47 +0700

    feat: Create Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
+import Hero from "../components/Landing/hero";
+import NavbarLanding from "../components/Landing/navbar";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="">
+      <NavbarLanding />
+      <Hero />
+    </main>
+  );
 }
