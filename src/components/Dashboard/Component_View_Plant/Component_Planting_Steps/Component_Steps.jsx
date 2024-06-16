843584b2463f7d1c5bec405e14f95c3480d73f88
BangOns <syahr9950@gmail.com>
2024-06-16 11:10:05 +0700

    fix: Fix in Component Step in FInshed in add plant and table to deploy

--- src/components/Dashboard/Component_View_Plant/Component_Planting_Steps/Component_Steps.jsx
+++ src/components/Dashboard/Component_View_Plant/Component_Planting_Steps/Component_Steps.jsx
@@ -32,8 +32,8 @@ export default function Component_Steps({ dataPlantsStep }) {
             <Image
               src={
                 dataPlantsStep.step_image_url
-                  ? ImageImport.ImagePlants
-                  : dataPlantsStep.step_image_url
+                  ? dataPlantsStep.step_image_url
+                  : ImageImport.ImagePlants
               }
               width={60}
               height={60}
