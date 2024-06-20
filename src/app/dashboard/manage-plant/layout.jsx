275bda1a05cb8a5d07f2289a3432ad10c4a60bea
BangOns <syahr9950@gmail.com>
2024-06-20 21:56:23 +0700

    fix: Fix in Page edit

--- src/app/dashboard/manage-plant/layout.jsx
+++ src/app/dashboard/manage-plant/layout.jsx
@@ -1,20 +1,11 @@
 "use client";
 import React from "react";
 import Header from "@/components/Dashboard/header";
-import { useSelector } from "react-redux";
 export default function layout({ children }) {
-  const { isMinimizeds } = useSelector((state) => state.dashboard);
-
   return (
-    <main
-      className={`${
-        !isMinimizeds ? "xl:ml-56 max-xl:ml-20" : "xl:ml-24 max-xl:ml-5"
-      }`}
-    >
-      <article className="pl-5 pr-10 bg-white h-full min-h-screen">
-        <Header />
-        {children}
-      </article>
-    </main>
+    <>
+      <Header />
+      {children}
+    </>
   );
 }
